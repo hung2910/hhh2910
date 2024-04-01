109601134406b31879b9cabf9d36ee96f10d9012
hung2910 <165612577+hung2910@users.noreply.github.com>
2024-04-01 10:07:50 +0700

    Add files via upload

---
+++ BTM2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+// Hàm kiểm tra số chính phương
+bool isPerfectSquare(int number) {
+	int squareRoot = sqrt(number);
+	return squareRoot * squareRoot == number;
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void countAndPrintPerfectSquares(int n) {
+	std::cout << "Cac so chinh phuong nho hon " << n << " la: ";
+	int count = 0;
+	for (int i = 1; i < n; i++) {
+		if (isPerfectSquare(i)) {
+			count++;
+			std::cout << i << " ";
+		}
+	}
+	std::cout << "\nTong cong co " << count << " so chinh phuong." << std::endl;
+}
+
+int main() {
+	int n;
+	std::cout << "Nhap vao mot so nguyen duong n: ";
+	std::cin >> n;
+	countAndPrintPerfectSquares(n);
+	return 0;
+}
